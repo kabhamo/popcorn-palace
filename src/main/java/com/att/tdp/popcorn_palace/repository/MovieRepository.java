d09047bed35b51231d6782e5d5428a95f88bcdd6
kabha <mkabha54@gmail.com>
2025-03-18 18:50:50 +0200

    refactor(repository): update repository methods

--- src/main/java/com/att/tdp/popcorn_palace/repository/MovieRepository.java
+++ src/main/java/com/att/tdp/popcorn_palace/repository/MovieRepository.java
@@ -4,14 +4,10 @@ import com.att.tdp.popcorn_palace.model.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long> {
 
-    List<Movie> findByGenre(String genre);
-
-    List<Movie> findByReleaseYear(int releaseYear);
-
-    List<Movie> findByTitleContaining(String keyword);
+    Optional<Movie> findByTitle(String title);
 }
